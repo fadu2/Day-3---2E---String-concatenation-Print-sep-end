ecaa8476e55901480721a638a1bf2e6af797f0b8
FrancisOhemengA <93487638+fadu2@users.noreply.github.com>
2021-11-02 19:48:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+# Warm up
+# Print this:
+#1
+# print("John said, \"Joe’s book is very interesting\"")
+
+# print('John said, "Joe\'s book is very interesting"')
+
+# print("""John said, "Joe's book is very interesting" """)
+
+
+# #2
+# print("/\\_\\_\\_\\\n|      |\n|______|")
+
+# String concatenation 
+# print("hello" + "world")
+# print("hello " + "world")
+# print("hello" + " world")
+# print("hello" + " " + "world")
+# word1 = "hello"
+# word2 = "world"
+# print(word1 + word2)
+# print(word1 + " " + word2)
+# first_name = "John"
+# last_name = "Smith"
+# print(first_name + " " + last_name)
+# print(first_name,last_name) # not concatenation
+
+# a = 2
+# print("hello" + str(a))
+# print(1 + a)
+# print("hello" + str(1 + a))
+# print("hello", 2)
+
+# print end 
+print("one")
+print("two")
+print("three")
+
+print("one", end=" ")
+print("two", end=" ")
+print("three")
+
+print("one", end="\n")
+print("two", end="\n")
+print("three")
+
+print("one", end="**")
+print("two", end="**")
+print("three", end="**\n")
+
+# print sep -- separation
+print("one", "two", "three")
+print("one", "two", "three", sep="|")
+
+month = "09"
+day = "21"
+year = "2021"
+# 09/21/2021
+print(month, day, year, sep="/")
+# 09-21-2021
+print(month, day, year, sep="-")
+
+'''
+***My extra stuff***
+a = "adu\b"
+b = "adu"
+print(a + b)
+print("/\\\\\\\\\\\\\n|     |\n|_____|")
+
+'''
+
+print(“hello world”)
